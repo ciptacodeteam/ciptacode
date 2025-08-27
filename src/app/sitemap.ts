aa38fa3d963935f9bb366ef6cfd617f843dfb092
Juan Antonio <juananthonio23@gmail.com>
2025-08-27 16:29:07 +0700

    feat: remove outdated images and add sitemap functionality for improved SEO

---
+++ src/app/sitemap.ts
@@ -0,0 +1,39 @@
+import type { MetadataRoute } from "next";
+
+const baseUrl = "https://ciptacode.id";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+      images: [`${baseUrl}/images/home.webp`],
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/portfolio`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/services`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.85,
+    },
+  ];
+}
